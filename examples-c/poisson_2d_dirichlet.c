5c5c93bd045cd04bf7844c008778f24b06d27944
Quentin Oschatz <q.oschatz@gmail.com>
2024-09-04 17:04:09 -0400

    FEAT: Add dirichlet example

--- examples-c/poisson_2d_dirichlet.c
+++ examples-c/poisson_2d_dirichlet.c
@@ -4,9 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 8 // Size of the grid (must be a power of 2)
+#include <complex.h>
+
+#define N 15 // Size of the grid (2*N+2 must be a power of 2)
 #define L 1.0 // Physical size of the domain
-#define SIG 0.1 // Width << 1
+#define SIG 0.2 // Width << 1
 
 void initialize_rhs(float ***rhs, int n, float l) {
     for (int i = 1; i <= n; i++) {
@@ -19,34 +21,52 @@ void initialize_rhs(float ***rhs, int n, float l) {
     }
 }
 
-void sine_fft_2d(float ***data, int n) {
+void create_f_ext(float ***data, float ***f_ext, int n) {
 
-	for (int i = 1; i <= n; i++) {
-
-		sinft(data[1][i], n);
-
-	}
-	
-	float *temp_data = vector(1, n);
-	for (int i = 1; i <= n; i++) {
-
-		for (int j = 1; j <= n; j++) {
-			
-			temp_data[j] = data[1][j][i];
-
+	for (int i = 2; i <= n+1; i++) {
+		for (int j = 2; j <= n+1; j++) {
+			f_ext[1][i][j] = data[1][i-1][j-1];
 		}
+	}	
 
-		sinft(temp_data, n);
-
-		for (int j = 1; j <= n; j++) {
-			
-			data[1][j][i] = temp_data[j];
-
+	for (int i = n+3; i <= 2*n+2; i++) {
+		for (int j = 2; j <= n+1; j++) {
+			f_ext[1][i][j] = -data[1][n-(i-n-2)+1][j-1];
 		}
+	}	
 
+	for (int i = 2; i <= n+1; i++) {
+		for (int j = n+3; j <= 2*n+2; j++) {
+			f_ext[1][i][j] = -data[1][i-1][n-(j-n-2)+1];
+		}
 	}
 
-	free_vector(temp_data, 1, n);
+	for (int i = n+3; i <= 2*n+2; i++) {
+		for (int j = n+3; j <= 2*n+2; j++) {
+			f_ext[1][i][j] = data[1][n-(i-n-2)+1][n-(j-n-2)+1];
+		}
+	}	
+
+}
+
+void sine_fft_2d(float ***data, int n, int isign) {
+
+	float **speq = matrix(1, 1, 1, 4*n+4);
+	float ***f_ext = f3tensor(1, 1, 1, 2*n+2, 1, 2*n+2);
+
+	create_f_ext(data, f_ext, n);
+
+	rlft3(f_ext, speq, 1, 2*n+2, 2*n+2, 1);
+
+	for (int i = 2; i <= 1*n+1; i++) {
+		for (int j = 3; j <= 2*n+2; j+=2) {
+			if (isign > 0) data[1][i-1][(j-1)/2] = -f_ext[1][i][j] / 4;
+			else data[1][i-1][(j-1)/2] = -f_ext[1][i][j] * 4 / pow(N+1, 2.0);
+		}	
+	}
+
+	free_matrix(speq, 1, 1, 1, 4*n+4);
+	free_f3tensor(f_ext, 1, 1, 1, 2*n+2, 1, 2*n+2);
 
 }
 
@@ -54,7 +74,7 @@ int main() {
 
 	int i, j;
 
-	float ***data, **speq;
+	float ***data;
 	data = f3tensor(1, 1, 1, N, 1, N);
 	
 	float **phi = matrix(1, N, 1, N);   // Solution in real space
@@ -62,12 +82,20 @@ int main() {
 	// Initialize the data
 	initialize_rhs(data, N, L);
 
+	for (i = 1; i <= N; i++) {
+		for (j = 1; j <= N; j++) {
+			printf("%f ", data[1][i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
 	// Perform forward sine FFT
-	sine_fft_2d(data, N);
+	sine_fft_2d(data, N, 1);
 
 	// Solve Poisson equation in Fourier space
 	for (i = 1; i <= N; i++) {
-		for (j = 1; j <= N/2; j++) {
+		for (j = 1; j <= N; j++) {
 
 			float kx = (i-1 <= N / 2) ? i-1 : (i-1) - N;
 			float ky = (j-1 <= N / 2) ? j-1 : (j-1) - N;
@@ -77,19 +105,20 @@ int main() {
 
 			// At 0,0 don't normalize
 			if (k_squared != 0) {
-				data[1][i][2*(j-1)+1] /= k_squared;
-				data[1][i][2*(j-1)] /= k_squared;
-			}
+				data[1][i][j] /= k_squared;
+			} 
 		}
 	}
-	
+
+	data[1][1][1] = 0.0;
+
 	// Perform inverse sine FFT
-	sine_fft_2d(data, N);
+	sine_fft_2d(data, N, -1);
 
 	// Copy data array back to phi
 	for (i = 1; i <= N; i++) {
 		for (j = 1; j <= N; j++) {
-			phi[i][j] = data[1][i][j] * 2 / (N * N); // Normalize the solution
+			phi[i][j] = data[1][i][j] * 4 / ((N+1)*(N+1)); // Normalize the solution
 		}
 	}
 
